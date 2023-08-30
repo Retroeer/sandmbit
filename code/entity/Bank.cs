6bc26da674588de404dd0c544846890378da40be
cohaereo <cohaereo@protonmail.com>
2023-08-30 21:21:49 +0200

    Initial commit

---
+++ code/entity/Bank.cs
@@ -0,0 +1,22 @@
+﻿using Editor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.entity
+{
+	[Library( "gambit_bank" ), HammerEntity]
+	[Title( "Bank" ), Category( "Gambit" ), Icon( "place" )]
+	[Model]
+	public class Bank : InteractibleEntity
+	{
+		public Bank() : base( "models/sambit/bank.vmdl" ) { }
+
+		public override void Spawn()
+		{
+			SetupPhysicsFromModel( PhysicsMotionType.Static );
+		}
+	}
+}
---
+++ code/entity/Mote.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.entity
+{
+	[Library( "gambit_mote", Title = "Mote" )]
+	class Mote : ModelEntity
+	{
+		public Mote() : base() {
+			Model = Cloud.Model( "destiny.gambit_mote" );
+		}
+
+		public override void Spawn()
+		{
+			// Always network this entity to all clients
+			Transmit = TransmitType.Always;
+
+			Scale = 1.25f;
+			SetupPhysicsFromModel( PhysicsMotionType.Dynamic );
+
+			EnableAllCollisions = true;
+			EnableSolidCollisions = true;
+			PhysicsEnabled = true;
+			UsePhysicsCollision = true;
+
+			Tags.Add( "trigger" );
+		}
+	}
+}
